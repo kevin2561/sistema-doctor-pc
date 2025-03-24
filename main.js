8fb7de386a95baa8f97f6a1e622a61350bc6991a
KEVIN NOA ROMERO <kevinnoa02@gmail.com>
2025-03-23 20:47:19 -0500

    version 1.1.3

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.whenReady().then(() => {
   });
 
   // mainWindow.setMenu(null);
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   globalShortcut.register('Ctrl+Shift+I', () => {
     mainWindow.webContents.openDevTools();
